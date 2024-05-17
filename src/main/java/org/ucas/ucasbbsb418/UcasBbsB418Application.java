da674c4b1cfa2b81c9e2697426e6a89ca8809a85
zlDing <zl-ding@outlook.com>
2024-05-17 11:25:59 +0800

    init

---
+++ src/main/java/org/ucas/ucasbbsb418/UcasBbsB418Application.java
@@ -0,0 +1,13 @@
+package org.ucas.ucasbbsb418;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcasBbsB418Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcasBbsB418Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/ucas/ucasbbsb418/UcasBbsB418ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ucas.ucasbbsb418;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcasBbsB418ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
